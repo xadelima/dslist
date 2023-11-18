ec5251e565589cda284cf8a8ff3808631bdfdce4
Alexandre <Alexandre@livenyo.com>
2023-11-18 19:04:59 -0300

    Model Domain

---
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+    private  String name;
+
+    public GameList(){
+    }
+    public GameList(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GameList gamelist)) return false;
+        return getId() == gamelist.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
